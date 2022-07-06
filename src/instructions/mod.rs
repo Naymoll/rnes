15acf8e32985df24c4fac724398e9e794926a807
Naymoll <xdarkknight122x@gmail.com>
2022-07-06 21:44:02 +0300

    Add error type for instructions

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,3 +1,5 @@
+use thiserror::Error;
+
 pub struct Context;
 
 pub trait Instruction: TryFrom<u8> {
@@ -8,4 +10,17 @@ pub trait Instruction: TryFrom<u8> {
     fn opcode(&self) -> u8;
     fn len(&self) -> u8;
     fn cycle(&self) -> u8;
+}
+
+#[derive(Error, Debug)]
+#[error("invalid opcode (expected {expected}, found {found})")]
+pub struct InvalidOpcodeError {
+    expected: u8,
+    found: u8,
+}
+
+impl InvalidOpcodeError {
+    pub fn new(expected: u8, found: u8) -> Self {
+        Self { expected, found }
+    }
 }
--- src/macros/mod.rs
+++ src/macros/mod.rs
@@ -3,21 +3,28 @@ macro_rules! instruction {
         pub struct $name;
 
         impl crate::instructions::Instruction for $name {
-            fn execute(&mut self, _ctx: &mut crate::instructions::Context) { $op }
-            fn opcode(&self) -> u8 { $opcode }
-            fn len(&self) -> u8 { $len }
-            fn cycle(&self) -> u8 { $cycle }
+            fn execute(&mut self, _ctx: &mut crate::instructions::Context) {
+                $op
+            }
+            fn opcode(&self) -> u8 {
+                $opcode
+            }
+            fn len(&self) -> u8 {
+                $len
+            }
+            fn cycle(&self) -> u8 {
+                $cycle
+            }
         }
 
         impl TryFrom<u8> for $name {
-            // TODO: Change error type
-            type Error = ();
+            type Error = crate::instructions::InvalidOpcodeError;
 
             fn try_from(opcode: u8) -> Result<Self, Self::Error> {
                 if opcode == $opcode {
                     Ok($name)
                 } else {
-                    Err(())
+                    Err(Self::Error::new($opcode, opcode))
                 }
             }
         }
